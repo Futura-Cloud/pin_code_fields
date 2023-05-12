c71b87f673dac6c1f53705f79884c19e382ac4f1
nickolight <nikolai.bezkorovainyi@gmail.com>
2023-05-12 19:34:27 +0300

    Revert "Made "platform.dart" file public"
    This reverts commit f2699e418350e4f2091ce5829e288af87e34850f.

--- lib/pin_code_fields.dart
+++ lib/pin_code_fields.dart
@@ -9,8 +9,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pin_code_fields/src/cursor_painter.dart';
-import 'package:pin_code_fields/src/models/platform.dart';
 
+part 'src/models/platform.dart';
 part 'src/models/haptic_feedback_type.dart';
 part 'src/models/animation_type.dart';
 part 'src/models/dialog_config.dart';
