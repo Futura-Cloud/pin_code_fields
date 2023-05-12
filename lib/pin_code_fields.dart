8cb13ea3e84bd561dbbdf1a18ffc545c244b173d
nickolight <nikolai.bezkorovainyi@gmail.com>
2023-05-12 19:31:28 +0300

    Made "platform.dart" file public

--- lib/pin_code_fields.dart
+++ lib/pin_code_fields.dart
@@ -10,7 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pin_code_fields/src/cursor_painter.dart';
 
-part '/src/models/platform.dart';
+part 'src/models/platform.dart';
 part 'src/models/haptic_feedback_type.dart';
 part 'src/models/animation_type.dart';
 part 'src/models/dialog_config.dart';
